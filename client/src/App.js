30b2d9371993d36fed44c5dde0a861e0f9931e86
avgur89 <avgur89@gmail.com>
2019-12-22 15:03:43 +0200

    Add login register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,39 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactState from './context/contact/contactState';
 import AuthState from './context/auth/authState';
+import AlertState from './context/alert/alertState';
 import Navbar from './components/layout/Navbar';
+import Alerts from './components/layout/Alerts';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => {
-    return (
-        <AuthState>
-            <ContactState>
-                <Router>
-                    <Fragment>
-                        <Navbar />
-                        <div className='container'>
-                            <Switch>
-                                <Route exact path='/' component={Home} />
-                                <Route exact path='/about' component={About} />
-                            </Switch>
-                        </div>
-                    </Fragment>
-                </Router>
-            </ContactState>
-        </AuthState>
-    );
+  return (
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/about" component={About} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
+  );
 };
 
 export default App;
